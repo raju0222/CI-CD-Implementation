1e3fc6bde2ec83911545d087185899cadf2173a8
Tumma <gt250108@ncr.com>
2019-11-12 16:24:18 +0530

    initial commit

---
+++ src/main/java/com/cicd/cicdimp/CicdimpApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.cicdimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdimpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdimpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicd/cicdimp/CicdimpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.cicdimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdimpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
